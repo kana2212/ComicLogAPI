2313df1ef5cb0a618c2e49a7616c6945bf1a5b21
kana2212 <reki2shi@yahoo.co.jp>
2023-11-27 11:12:17 +0900

    id検索機能を実装

--- src/main/java/com/example/Comic_Log_API/controller/ComicLogController.java
+++ src/main/java/com/example/Comic_Log_API/controller/ComicLogController.java
@@ -3,6 +3,7 @@ package com.example.Comic_Log_API.controller;
 import com.example.Comic_Log_API.entity.Comics;
 import com.example.Comic_Log_API.service.ComicLogService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,8 +15,14 @@ public class ComicLogController {
     public ComicLogController(ComicLogService comicLogService) {
         this.comicLogService = comicLogService;
     }
-        @GetMapping("/comiclogs")
-        public List<Comics> findAll() {
-            return comicLogService.findAll();
-        }
+
+    @GetMapping("/comiclogs")
+    public List<Comics> findAll() {
+        return comicLogService.findAll();
     }
+
+    @GetMapping("/comiclogs/{id}")
+    public Comics getComics(@PathVariable("id") Integer id) {
+        return comicLogService.findById(id);
+    }
+}
--- src/main/java/com/example/Comic_Log_API/repository/ComicLogMapper.java
+++ src/main/java/com/example/Comic_Log_API/repository/ComicLogMapper.java
@@ -5,10 +5,14 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Optional;
+
 @Mapper
 public interface ComicLogMapper {
 
         @Select("SELECT * FROM comics")
         List<Comics> findAll();
 
+        @Select("SELECT * FROM comics WHERE id = #{id}")
+        Optional<Comics> findById(Integer id);
 }
