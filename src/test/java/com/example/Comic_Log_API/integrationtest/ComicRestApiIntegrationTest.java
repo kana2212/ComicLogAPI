4765c738511aae8452a673bae72d62cc75b97897
kana2212 <reki2shi@yahoo.co.jp>
2024-01-05 18:34:35 +0900

    テスト内容修正

--- src/test/java/com/example/Comic_Log_API/integrationtest/ComicRestApiIntegrationTest.java
+++ src/test/java/com/example/Comic_Log_API/integrationtest/ComicRestApiIntegrationTest.java
@@ -1,8 +1,7 @@
 package com.example.Comic_Log_API.integrationtest;
 
-import com.github.database.rider.core.api.dataset.DataSet;
-import com.github.database.rider.core.api.dataset.ExpectedDataSet;
-import com.github.database.rider.spring.api.DBRider;
+import java.nio.charset.StandardCharsets;
+
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
@@ -16,9 +15,9 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.nio.charset.StandardCharsets;
-
-
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
+import com.github.database.rider.spring.api.DBRider;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,124 +25,96 @@ import java.nio.charset.StandardCharsets;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class ComicRestApiIntegrationTest {
 
-    @Autowired
-    MockMvc mockMvc;
+   @Autowired
+   MockMvc mockMvc;
 
-    @Test
-    @DataSet(value = "datasets/comicList.yml")
-    @Transactional
-    void コミック情報を全件取得した際にステータスコードが200を返すこと() throws Exception {
-        String response = mockMvc.perform(MockMvcRequestBuilders.get("/comiclogs"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+   @Test
+   @DataSet(value = "datasets/comicList.yml")
+   @Transactional
+   void 指定したIDのコミック情報を取得した際にステータスコードが200を返すこと() throws Exception {
+      String response = mockMvc.perform(MockMvcRequestBuilders.get("/comiclogs/1"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
 
-        JSONAssert.assertEquals("""
-                    [
-                    {
-                    "id": 1,
-                    "comicServiceName": "コミックシーモア",
-                    "comicTitle": "鬼滅の刃",
-                    "volumes": 22
-                    },
-                    {
-                    "id": 2,
-                    "comicServiceName": "Rakuten Kobo",
-                    "comicTitle": "薬屋のひとりごと",
-                    "volumes": 4
-                    },
-                    {
-                    "id": 3,
-                    "comicServiceName": "Kindle",
-                    "comicTitle": "ダンジョン飯",
-                    "volumes": 12
-                    }
-                    ]
-                """, response, JSONCompareMode.STRICT);
-    }
-    @Test
-    @DataSet(value = "datasets/comicList.yml")
-    @Transactional
-    void 指定したIDのコミック情報を取得した際にステータスコードが200を返すこと() throws Exception {
-        String response = mockMvc.perform(MockMvcRequestBuilders.get("/comiclogs/1")).
-                andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+      JSONAssert.assertEquals("""
+              {
+               "id": 1,
+               "comicServiceName": "コミックシーモア",
+               "comicTitle": "鬼滅の刃",
+               "volumes": 23,
+               "status": "完結"
+            }
+             """, response, JSONCompareMode.STRICT);
+   }
 
-        JSONAssert.assertEquals("""
-                 {
-                "id": 1,
-                    "comicServiceName": "コミックシーモア",
-                    "comicTitle": "鬼滅の刃",
-                    "volumes": 22
-                 }
-                """, response, JSONCompareMode.STRICT);
-    }
+   @Test
+   @DataSet(value = "datasets/comicList.yml")
+   @Transactional
+   void 存在しないIDを指定してコミック情報を取得するときに例外が返されること() throws Exception {
+      mockMvc.perform(MockMvcRequestBuilders.get("/comiclogs/99"))
+            .andExpect(MockMvcResultMatchers.status().isNotFound());
+   }
 
-    @Test
-    @DataSet(value = "datasets/comicList.yml")
-    @Transactional
-    void 存在しないIDを指定してコミック情報を取得するときに例外が返されること() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/comiclogs/99"))
-                .andExpect(MockMvcResultMatchers.status().isNotFound());
-    }
-    @Test
-    @DataSet(value = "datasets/comicList.yml")
-    @ExpectedDataSet(value = "datasets/insert_comic.yml", ignoreCols = "id")
-    @Transactional
-    void コミック情報を新規登録できること() throws Exception {
-        String response = mockMvc.perform(MockMvcRequestBuilders.post("/comiclogs")
-                        .contentType(MediaType.APPLICATION_JSON).content("""
-                                {
-                                   "id": 4,
-                                   "comicServiceName": "DMMブックス",
-                                   "comicTitle": "月刊少女野崎くん",
-                                   "volumes": 13
-                                }
-                                """))
-                .andExpect(MockMvcResultMatchers.status().isCreated())
-                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+   @Test
+   @DataSet(value = "datasets/comicList.yml")
+   @ExpectedDataSet(value = "datasets/insert_comic.yml", ignoreCols = "id")
+   @Transactional
+   void コミック情報を新規登録できること() throws Exception {
+      String response = mockMvc.perform(MockMvcRequestBuilders.post("/comiclogs")
+            .contentType(MediaType.APPLICATION_JSON).content("""
+                  { "id": 4,
+                  "comicServiceName": "DMMブックス",
+                  "comicTitle": "月刊少女野崎くん",
+                  "volumes": 13,
+                  "status": "続"
+                  }
+                  """))
+            .andExpect(MockMvcResultMatchers.status().isCreated())
+            .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
 
-        JSONAssert.assertEquals("""
-                {
-                   "message": "successfully created"
-                }
-                """, response, JSONCompareMode.STRICT);
-    }
-    @Test
-    @DataSet(value = "datasets/comicList.yml")
-    @ExpectedDataSet(value = "datasets/update_comic.yml")
-    @Transactional
-    void 指定したIDのコミック情報を更新できること() throws Exception {
-        String response = mockMvc.perform(MockMvcRequestBuilders.patch("/comiclogs/2")
-                        .contentType(MediaType.APPLICATION_JSON).content("""
-                                {
-                                   "id": 2,
-                                   "comicServiceName": "Rakuten Kobo",
-                                   "comicTitle": "薬屋のひとりごと",
-                                   "volumes": 11
-                                }
-                                """))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+      JSONAssert.assertEquals("""
+            {
+               "message": "successfully created"
+            }
+            """, response, JSONCompareMode.STRICT);
+   }
 
-        JSONAssert.assertEquals("""
-                {
-                   "message": "successfully updated"
-                }
-                """, response, JSONCompareMode.STRICT);
-    }
-    @Test
-    @DataSet(value = "datasets/comicList.yml")
-    @ExpectedDataSet(value = "datasets/delete_comic.yml")
-    @Transactional
-    void コミック情報を1件削除できること() throws Exception {
-        String response = mockMvc.perform(MockMvcRequestBuilders.delete("/comiclogs/3"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+   @Test
+   @DataSet(value = "datasets/comicList.yml")
+   @ExpectedDataSet(value = "datasets/update_comic.yml")
+   @Transactional
+   void 指定したIDのコミック情報を更新できること() throws Exception {
+      String response = mockMvc.perform(MockMvcRequestBuilders.patch("/comiclogs/2")
+            .contentType(MediaType.APPLICATION_JSON).content("""
+                  { "id": 2,
+                  "comicServiceName": "Rakuten Kobo",
+                  "comicTitle": "薬屋のひとりごと",
+                  "volumes": 11,
+                  "status": "続"
+                  """))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
 
-        JSONAssert.assertEquals("""
-                {
-                "message": "successfully deleted"
-                }
-                """, response, JSONCompareMode.STRICT);
-    }
+      JSONAssert.assertEquals("""
+            {
+               "message": "successfully updated"
+            }
+            """, response, JSONCompareMode.STRICT);
+   }
+
+   @Test
+   @DataSet(value = "datasets/comicList.yml")
+   @ExpectedDataSet(value = "datasets/delete_comic.yml")
+   @Transactional
+   void コミック情報を1件削除できること() throws Exception {
+      String response = mockMvc.perform(MockMvcRequestBuilders.delete("/comiclogs/3"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+      JSONAssert.assertEquals("""
+            {
+            "message": "successfully deleted"
+            }
+            """, response, JSONCompareMode.STRICT);
+   }
 }
--- src/test/java/com/example/Comic_Log_API/service/ComicLogServiceImplTest.java
+++ src/test/java/com/example/Comic_Log_API/service/ComicLogServiceImplTest.java
@@ -1,8 +1,11 @@
 package com.example.Comic_Log_API.service;
 
-import com.example.Comic_Log_API.entity.Comic;
-import com.example.Comic_Log_API.exception.ResourceNotFoundException;
-import com.example.Comic_Log_API.repository.ComicLogMapper;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -10,16 +13,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.List;
-import java.util.Optional;
-
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import com.example.Comic_Log_API.entity.Comic;
+import com.example.Comic_Log_API.exception.ResourceNotFoundException;
+import com.example.Comic_Log_API.repository.ComicLogMapper;
 
 @ExtendWith(MockitoExtension.class)
 public class ComicLogServiceImplTest {
@@ -29,23 +25,11 @@ public class ComicLogServiceImplTest {
     ComicLogMapper comicLogMapper;
 
     @Test
-    public void コミックの情報が全権取得できること() throws Exception {
-        List<Comic> comicList = List.of(
-                new Comic("コミックシーモア", "鬼滅の刃", 22),
-                new Comic("Renta!", "葬送のフリーレン", 11),
-                new Comic("ダンジョン飯", "Kindle", 12));
-        doReturn(comicList).when(comicLogMapper).findAll();
-        List<Comic> actual = comicLogServiceImpl.findAll();
-        assertThat(actual).isEqualTo(comicList);
-        verify(comicLogMapper, times(1)).findAll();
-    }
-
-    @Test
     public void 存在するIDを指定した時に正常にコミックの情報が返されること() {
-        doReturn(Optional.of(new Comic("コミックシーモア", "鬼滅の刃", 22))).when(comicLogMapper).findById(1);
+        doReturn(Optional.of(new Comic("コミックシーモア", "鬼滅の刃", 23, "完結"))).when(comicLogMapper).findById(1);
 
         Comic actual = comicLogServiceImpl.findById(1);
-        assertThat(actual).isEqualTo(new Comic("コミックシーモア", "鬼滅の刃", 22));
+        assertThat(actual).isEqualTo(new Comic("コミックシーモア", "鬼滅の刃", 23, "完結"));
         verify(comicLogMapper, times(1)).findById(1);
     }
 
@@ -60,31 +44,32 @@ public class ComicLogServiceImplTest {
 
     @Test
     public void 新規にコミック情報を登録できること() {
-        Comic testComic = new Comic("楽天ブックス", "薬屋のひとりごと", 11);
-        comicLogServiceImpl.createComics("楽天ブックス", "薬屋のひとりごと", 11);
+        Comic testComic = new Comic("楽天ブックス", "薬屋のひとりごと", 11, "続");
+        comicLogServiceImpl.createComics("楽天ブックス", "薬屋のひとりごと", 11, "続");
         Mockito.verify(comicLogMapper).createComics(testComic);
     }
 
     @Test
     public void 指定したIDのコミック情報を更新できること() {
-        Comic updateComic = new Comic("コミックシーモア", "鬼滅の刃", 23);
-        doReturn(Optional.of(new Comic("コミックシーモア", "鬼滅の刃", 23)))
-                .when(comicLogMapper).findById(1);
+        Comic updateComic = new Comic("Renta!", "葬送のフリーレン", 11, "続");
+        doReturn(Optional.of(new Comic("Renta!", "葬送のフリーレン", 11, "続")))
+                .when(comicLogMapper).findById(2);
 
-        comicLogServiceImpl.updateComics(1, "コミックシーモア", "鬼滅の刃", 23);
-        verify(comicLogMapper, times(1)).findById(1);
-        verify(comicLogMapper, times(1)).updateComics(1, "コミックシーモア", "鬼滅の刃", 23);
+        comicLogServiceImpl.updateComics(2, "Renta!", "葬送のフリーレン", 12, "続");
+        verify(comicLogMapper, times(1)).findById(2);
+        verify(comicLogMapper, times(1)).updateComics(2, "Renta!", "葬送のフリーレン", 12, "続");
     }
 
     @Test
     public void 更新対象のIDが存在しない場合に例外をスローできること() throws Exception {
-        Comic updateComic = new Comic(60, "コミックフェスタ", "僕のヒーローアカデミア", 35);
+        Comic updateComic = new Comic(60, "コミックフェスタ", "僕のヒーローアカデミア", 35, "続");
 
-        assertThatThrownBy(() -> comicLogServiceImpl.updateComics(60, "コミックフェスタ", "僕のヒーローアカデミア", 35))
+        assertThatThrownBy(() -> comicLogServiceImpl.updateComics(60, "コミックフェスタ", "僕のヒーローアカデミア", 35, "続"))
                 .isInstanceOf(ResourceNotFoundException.class);
-        verify(comicLogMapper, never()).updateComics(60, "コミックフェスタ", "僕のヒーローアカデミア", 35);
+        verify(comicLogMapper, never()).updateComics(60, "コミックフェスタ", "僕のヒーローアカデミア", 35, "続");
 
     }
+
     @Test
     public void 指定したIDを削除できること() {
         comicLogServiceImpl.deleteComics(1);
