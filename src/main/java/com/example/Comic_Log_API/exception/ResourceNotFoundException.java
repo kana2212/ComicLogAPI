bf90c58e4bd81f5bc380642251e8a15d00bcfbe4
kana2212 <reki2shi@yahoo.co.jp>
2024-01-28 20:58:41 +0900

    誤ったファイルを削除

--- src/main/java/com/example/Comic_Log_API/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.comic_log_api.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-
-}
